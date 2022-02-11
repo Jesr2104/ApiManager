0df48c798eee61c2b48465c328900a23149c9c6c
Jorge Eliecer Soto Ramos <36306814+Jesr2104@users.noreply.github.com>
2022-02-11 06:21:59 +0000

    version: 1.03

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 import pyrebase
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 
 
 # function to configurate our service of database
@@ -18,7 +18,7 @@ def config_firebase():
         "appId": "1:496229318920:web:98f64838f81f9744a32cbd"
     }
     firebase = pyrebase.initialize_app(firebase_config)
-    return firebase.database()
+    return firebase
 
 
 # function to configurate our service of database
@@ -46,7 +46,7 @@ def create_new_product(moq, categoryProduct, discount, imageProduct,
 # function to get the list of the product on the API
 # ============================================================================
 def get_list_of_product():
-    my_db = config_firebase()
+    my_db = config_firebase().database()
     data = my_db.child("productsDb").get()
     return data.val()
 
@@ -55,15 +55,25 @@ def get_list_of_product():
 # ============================================================================
 def insert_new_product(newItem):
     # create the instance of the Firebase to insert the new item
-    my_db = config_firebase()
+    my_db = config_firebase().database()
 
     # this function get the list from the server insert de new item
     # and save the complete list en the server.
     product_list = get_list_of_product()
-    product_list.append(newItem)
+    if product_list is not None:
+        product_list.append(newItem)
+    else:
+        product_list = list()
+        product_list.append(newItem)
 
     my_db.child("productsDb").set(product_list)
     return True
+
+
+# function to get the file of the product
+# ============================================================================
+def get_file_image():
+    return QFileDialog.getOpenFileName()[0]
 
 
 # class to show the graphic windows and interface
@@ -83,10 +93,14 @@ class InterfaceGui(QMainWindow):
         # event to the creation of the new product
         self.button_create_product.clicked.connect(self.form_insert_product)
 
+        # event to set the image of the product
+        self.button_load_image.clicked.connect(lambda: self.editText_image_location.setText(get_file_image()))
+
         # set the default options
         self.available_option_true.setChecked(True)
         self.disable_option_false.setChecked(True)
         self.season_option_false.setChecked(True)
+        self.editText_image_location.setEnabled(False)
 
     def form_insert_product(self):
         # Text Field
@@ -109,38 +123,55 @@ class InterfaceGui(QMainWindow):
         # Image
         image = self.editText_image_location.text()
 
-        # check field information
-        if product_name != "" and \
-                product_origen != "" and \
-                price != "" and \
-                discount != "" and \
-                moq != "" and \
-                image != "" and \
-                not (category == "Select category") and \
-                not (sales_unit == "Select option"):
-
-            # if all fields are full we insert the new product
-            new_product = create_new_product(
-                nameProduct=product_name,
-                productOrigin=product_origen,
-                price=price,
-                discount=discount,
-                moq=moq,
-                productDetails=description,
-                categoryProduct=category,
-                salesUnit=sales_unit,
-                isAvailable=available,
-                isDisable=disable,
-                inSeason=season_product,
-                imageProduct=image
-            )
-            if insert_new_product(new_product):
-                print("The operation successful!!")
-                self.clean_form()
+        if image != "":
+            image_url = self.load_image()
+            if image_url == "":
+                self.show_dialog("error...", "There was an error loading the image     ")
             else:
-                print("The operation failed..")
+                # check field information
+                if product_name != "" and \
+                        product_origen != "" and \
+                        price != "" and \
+                        discount != "" and \
+                        moq != "" and \
+                        not (category == "Select category") and \
+                        not (sales_unit == "Select option"):
+
+                    # if all fields are full we insert the new product
+                    new_product = create_new_product(
+                        nameProduct=product_name,
+                        productOrigin=product_origen,
+                        price=price,
+                        discount=discount,
+                        moq=moq,
+                        productDetails=description,
+                        categoryProduct=category,
+                        salesUnit=sales_unit,
+                        isAvailable=available,
+                        isDisable=disable,
+                        inSeason=season_product,
+                        imageProduct=image_url
+                    )
+                    if insert_new_product(new_product):
+                        self.clean_form()
+                        self.show_dialog("Completed      ", "The operation successful!!      ")
+                    else:
+                        self.show_dialog("Error has occurred      ", "The operation failed..      ")
+                else:
+                    self.show_dialog("Information is missing      ", "Some fields are empty      ")
         else:
-            print("some fields are empty")
+            self.show_dialog("image is missing      ", "Product image not selected      ")
+
+    def load_image(self):
+        store = config_firebase().storage()
+        title_file = self.split_name(self.editText_image_location.text())
+        result = store.child('imagenProduct/' + title_file).put(self.editText_image_location.text())
+
+        return store.child('imagenProduct/' + title_file).get_url(result['downloadTokens'])
+
+    def split_name(self, url):
+        x = url.split("/")
+        return x[len(x) - 1]
 
     def clean_form(self):
         self.editText_product_name.setText("")
@@ -150,6 +181,12 @@ class InterfaceGui(QMainWindow):
         self.editText_moq.setText("")
         self.editText_description.setText("")
         self.editText_image_location.setText("")
+
+    def show_dialog(self, title, message):
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle(title)
+        dlg.setText(message)
+        dlg.exec()
 
 
 # function main
